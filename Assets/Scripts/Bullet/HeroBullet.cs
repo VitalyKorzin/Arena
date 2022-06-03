cbbf08fcff310d062a6803526d790d1c83f3d4e3
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-06-03 13:30:14 +0300

    Bullet classes created

---
+++ Assets/Scripts/Bullet/HeroBullet.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class HeroBullet : Bullet
+{
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+        base.OnTriggerEnter2D(collision);
+    }
+}
