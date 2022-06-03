e2ddad01f49d9b28aebda9ba879a0998ee9e5e8c
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-06-03 13:07:38 +0300

    Pool object class created

---
+++ Assets/Scripts/ObjectsPool/PoolObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public abstract class PoolObject : MonoBehaviour
+{
+    [SerializeField] private uint _clonesCount;
+
+    public uint ClonesCount => _clonesCount;
+    public bool Deactivated => gameObject.activeSelf == false;
+
+    public void Activate() => gameObject.SetActive(true);
+
+    public void Deactivate() => gameObject.SetActive(false);
+}
