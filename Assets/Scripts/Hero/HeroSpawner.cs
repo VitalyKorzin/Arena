c47dc91430c6755ecc129237cf7eb226f5359fbd
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-06-03 18:26:52 +0300

    Hero spawner class created

---
+++ Assets/Scripts/Hero/HeroSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HeroSpawner : MonoBehaviour
+{
+    [SerializeField] private Hero hero;
+    [SerializeField] private Weapon weapon;
+    [SerializeField] private Transform _spawnPoint;
+    [SerializeField] private Transform _bulletsContainer;
+
+    public event UnityAction<Hero> Spawned;
+
+    private void OnEnable() => Validate();
+
+    private void Start()
+    {
+        hero = Instantiate(hero, _spawnPoint.position, Quaternion.identity);
+        weapon = Instantiate(weapon, hero.transform);
+        weapon.Initialize(_bulletsContainer);
+        hero.Initialize(weapon);
+        Spawned?.Invoke(hero);
+    }
+
+    private void Validate()
+    {
+        if (hero == null)
+            throw new InvalidOperationException();
+
+        if (weapon == null)
+            throw new InvalidOperationException();
+
+        if (_spawnPoint == null)
+            throw new InvalidOperationException();
+
+        if (_bulletsContainer == null)
+            throw new InvalidOperationException();
+    }
+}
