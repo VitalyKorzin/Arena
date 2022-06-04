f37740ed460b1d4b41daeaf52f07465c1ac23863
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-06-05 00:34:18 +0300

    Hero bullet logic added

--- Assets/Scripts/Bullet/HeroBullet.cs
+++ Assets/Scripts/Bullet/HeroBullet.cs
@@ -4,6 +4,12 @@ public class HeroBullet : Bullet
 {
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.TryGetComponent(out Enemy enemy))
+        {
+            enemy.Kill();
+            Deactivate();
+        }
+
         base.OnTriggerEnter2D(collision);
     }
 }
