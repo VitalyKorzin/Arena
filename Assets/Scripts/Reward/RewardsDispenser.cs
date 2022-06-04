cbd506399c3bfe558d4ade229e9fbe30ecd791a5
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-06-04 23:36:49 +0300

    Rewards dispenser created

---
+++ Assets/Scripts/Reward/RewardsDispenser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+public class RewardsDispenser : ObjectsPool<Reward>
+{
+    [SerializeField] private Transform _rewardsContainer;
+    [SerializeField] private EnemiesSpawner _enemiesSpawner;
+
+    protected override void OnEnable()
+        => _enemiesSpawner.Spawned += OnEnemySpawned;
+
+    private void OnDisable()
+        => _enemiesSpawner.Spawned -= OnEnemySpawned;
+
+    private void Awake() => Initialize(_rewardsContainer);
+
+    protected override void Validate()
+    {
+        base.Validate();
+
+        if (_rewardsContainer == null)
+            throw new InvalidOperationException();
+
+        if (_enemiesSpawner == null)
+            throw new InvalidOperationException();
+    }
+
+    private void OnEnemySpawned(Enemy enemy)
+        => enemy.Died += OnEnemyDied;
+
+    private void OnEnemyDied(Enemy enemy)
+    {
+        enemy.Died -= OnEnemyDied;
+        Dispense(enemy.transform);
+    }
+
+    private void Dispense(Transform dispensePoint)
+    {
+        if (TryGetRandomObject(out Reward reward))
+        {
+            reward.transform.SetPositionAndRotation(dispensePoint.position, Quaternion.identity);
+            reward.Activate();
+        }
+    }
+}
