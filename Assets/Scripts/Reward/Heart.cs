34a951ba6241a90c27f556f36ab5f6c01a2ea42b
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-06-04 23:31:59 +0300

    Reward classes created

---
+++ Assets/Scripts/Reward/Heart.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Heart : Reward
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Hero hero))
+        {
+            hero.Heal();
+            Deactivate();
+        }
+    }
+}
