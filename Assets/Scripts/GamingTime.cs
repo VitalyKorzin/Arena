048a07463423e37ab75ce3a3206ea5f3cefefdfb
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-06-05 19:02:07 +0300

    Gaming time class created

---
+++ Assets/Scripts/GamingTime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public class GamingTime : MonoBehaviour
+{
+    [SerializeField] private HeroSpawner _heroSpawner;
+
+    private Hero _hero;
+
+    private void OnEnable()
+    {
+        Validate();
+        _heroSpawner.Spawned += OnHeroSpawned;
+    }
+
+    private void OnDisable() => _heroSpawner.Spawned -= OnHeroSpawned;
+
+    public void Play() => Time.timeScale = 1;
+
+    public void Pause() => Time.timeScale = 0;
+
+    private void OnHeroSpawned(Hero hero)
+    {
+        _hero = hero != null ? hero : throw new InvalidOperationException();
+        _hero.Died += OnHeroDied;
+        Play();
+    }
+
+    private void OnHeroDied()
+    {
+        _hero.Died -= OnHeroDied;
+        Pause();
+    }
+
+    private void Validate()
+    {
+        if (_heroSpawner == null)
+            throw new InvalidOperationException();
+    }
+}
