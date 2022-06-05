593a5e457bebdd9e94dc017bcfa60a4c46371091
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-06-05 21:58:38 +0300

    Score display class created

---
+++ Assets/Scripts/UI/ValuesDisplay/ScoreDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    [SerializeField] private ScoreCounter _scoreCounter;
+    [SerializeField] private TMP_Text _valueDisplay;
+
+    private void OnEnable()
+    {
+        Validate();
+        _scoreCounter.ValueChanged += OnScoreChanged;
+    }
+
+    private void OnDisable() 
+        => _scoreCounter.ValueChanged -= OnScoreChanged;
+
+    private void OnScoreChanged(uint score)
+        => _valueDisplay.text = score.ToString();
+
+    private void Validate()
+    {
+        if (_scoreCounter == null)
+            throw new InvalidOperationException();
+
+        if (_valueDisplay == null)
+            throw new InvalidOperationException();
+    }
+}
