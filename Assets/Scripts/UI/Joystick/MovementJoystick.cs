d6e05ba3b24b77aa68c87e7c168445c0f2294bdd
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-06-05 18:08:34 +0300

    Joystick classes created

---
+++ Assets/Scripts/UI/Joystick/MovementJoystick.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class MovementJoystick : Joystick
+{
+    [SerializeField] private ShootingJoystick _shootingJoystick;
+
+    private void Update()
+    {
+        if (_shootingJoystick.IsUsing == false)
+            Hero.TakeAim(Direction);
+    }
+
+    private void FixedUpdate() => Hero.Move(Direction);
+
+    protected override void Validate()
+    {
+        base.Validate();
+
+        if (_shootingJoystick == null)
+            throw new InvalidOperationException();
+    }
+}
---
+++ Assets/Scripts/UI/Joystick/ShootingJoystick.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShootingJoystick : Joystick, IBeginDragHandler
+{
+    private Coroutine _shootingJob;
+
+    private void Update()
+    {
+        if (IsUsing)
+            Hero.TakeAim(Direction);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+        => _shootingJob = StartCoroutine(Hero.Shoot());
+
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        if (_shootingJob != null)
+            StopCoroutine(_shootingJob);
+
+        base.OnEndDrag(eventData);
+    }
+}
