5e99428268ec029ce543939ac73759f88cd841d1
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-06-05 22:17:37 +0300

    Game over window class created

---
+++ Assets/Scripts/UI/Window/GameOverWindow.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class GameOverWindow : Window
+{
+    [SerializeField] private HeroSpawner _heroSpawner;
+    [SerializeField] private ScoreCounter _scoreCounter;
+    [SerializeField] private TMP_Text _scoreDisplay;
+    [SerializeField] private TMP_Text _coinsCountDisplay;
+
+    private RewardsCollector _collector;
+    private Hero _hero;
+
+    private void OnEnable()
+    {
+        Validate();
+        _heroSpawner.Spawned += OnHeroSpawned;
+    }
+
+    private void OnDisable()
+    {
+        _hero.Died -= OnHeroDied;
+        _heroSpawner.Spawned -= OnHeroSpawned;
+    }
+
+    private void OnHeroSpawned(Hero hero)
+    {
+        _hero = hero != null ? hero : throw new InvalidOperationException();
+        _hero.Died += OnHeroDied;
+
+        if (_hero.gameObject.TryGetComponent(out RewardsCollector collector))
+            _collector = collector;
+        else
+            throw new InvalidOperationException();
+    }
+
+    private void OnHeroDied()
+    {
+        Open();
+        _scoreDisplay.text = _scoreCounter.Value.ToString();
+        _coinsCountDisplay.text = _collector.Coins.ToString();
+    }
+
+    private void Validate()
+    {
+        if (_heroSpawner == null)
+            throw new InvalidOperationException();
+
+        if (_scoreCounter == null)
+            throw new InvalidOperationException();
+
+        if (_scoreDisplay == null)
+            throw new InvalidOperationException();
+
+        if (_coinsCountDisplay == null)
+            throw new InvalidOperationException();
+    }
+}
---
+++ Assets/Scripts/UI/Window/Window.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class Window : MonoBehaviour
+{
+    private CanvasGroup _canvasGroup;
+
+    private void Awake()
+    {
+        _canvasGroup = GetComponent<CanvasGroup>();
+        Close();
+    }
+
+    public void Open()
+    {
+        _canvasGroup.alpha = 1;
+        _canvasGroup.interactable = true;
+        _canvasGroup.blocksRaycasts = true;
+    }
+
+    public void Close()
+    {
+        _canvasGroup.alpha = 0;
+        _canvasGroup.interactable = false;
+        _canvasGroup.blocksRaycasts = false;
+    }
+}
