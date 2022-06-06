fd7be97d5b517beb748c11299227e1c50fb108c4
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-06-06 17:15:48 +0300

    Payment classes created

---
+++ Assets/Scripts/ScriptableObjects/Payment/CoinsPayment.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Payment/CoinsPayment", order = 51)]
+public class CoinsPayment : Payment
+{
+    public override void Buy(uint price, Buyer buyer)
+        => buyer.BuyPerCoins(price);
+
+    public override bool CheckSolvency(uint price, Buyer buyer)
+        => buyer.CheckSolvencyInCoins(price);
+}
