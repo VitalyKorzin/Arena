1c4b21a9a72a9310b24b35c31c067f3a0b06b393
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-06-06 15:26:07 +0300

    Saver class refactoring

--- Assets/Scripts/Saver/Saver.cs
+++ Assets/Scripts/Saver/Saver.cs
@@ -3,31 +3,35 @@ using UnityEngine;
 
 public abstract class Saver : MonoBehaviour
 {
+    private delegate T LoadObject<T>(string key);
+
     protected void SaveIntegerValue(string key, int value) 
         => PlayerPrefs.SetInt(key, value);
 
-    protected bool TryLoadIntegerValue(string key, out int result)
-    {
-        result = default;
-
-        if (PlayerPrefs.HasKey(key))
-            result = PlayerPrefs.GetInt(key);
-
-        return PlayerPrefs.HasKey(key) != false;
-    }
+    protected bool TryLoadIntegerValue(string key, out int result) 
+        => TryGetObject(PlayerPrefs.HasKey(key), key, LoadFromPlayerPrefs, out result);
 
     protected void SaveObject<T>(T obj, string fileName)
         => File.WriteAllText(GetPath(fileName), JsonUtility.ToJson(obj));
 
-    protected bool TryLoadObject<T>(string fileName, out T obj)
+    protected bool TryLoadObject<T>(string fileName, out T obj) 
+        => TryGetObject(File.Exists(GetPath(fileName)), fileName, LoadFromJsonUtility<T>, out obj);
+
+    private bool TryGetObject<T>(bool condition, string key, LoadObject<T> loadObject, out T result)
     {
-        obj = default;
+        result = default;
 
-        if (File.Exists(GetPath(fileName)))
-            obj = JsonUtility.FromJson<T>(File.ReadAllText(GetPath(fileName)));
+        if (condition)
+            result = loadObject(key);
 
-        return File.Exists(GetPath(fileName)) != false;
+        return condition != false;
     }
+
+    private T LoadFromJsonUtility<T>(string fileName) 
+        => JsonUtility.FromJson<T>(File.ReadAllText(GetPath(fileName)));
+
+    private int LoadFromPlayerPrefs(string key) 
+        => PlayerPrefs.GetInt(key);
 
     private string GetPath(string fileName)
         => Path.Combine(Application.dataPath, fileName);
