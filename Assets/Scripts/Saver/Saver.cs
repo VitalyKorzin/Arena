f518e497e5b568557ab850b7591d26f86220723b
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-06-06 14:26:23 +0300

    Saver abstract class created

---
+++ Assets/Scripts/Saver/Saver.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using UnityEngine;
+
+public abstract class Saver : MonoBehaviour
+{
+    protected void SaveIntegerValue(string key, int value) 
+        => PlayerPrefs.SetInt(key, value);
+
+    protected bool TryLoadIntegerValue(string key, out int result)
+    {
+        result = default;
+
+        if (PlayerPrefs.HasKey(key))
+            result = PlayerPrefs.GetInt(key);
+
+        return PlayerPrefs.HasKey(key) != false;
+    }
+
+    protected void SaveObject<T>(T obj, string fileName)
+        => File.WriteAllText(GetPath(fileName), JsonUtility.ToJson(obj));
+
+    protected bool TryLoadObject<T>(string fileName, out T obj)
+    {
+        obj = default;
+
+        if (File.Exists(GetPath(fileName)))
+            obj = JsonUtility.FromJson<T>(File.ReadAllText(GetPath(fileName)));
+
+        return File.Exists(GetPath(fileName)) != false;
+    }
+
+    private string GetPath(string fileName)
+        => Path.Combine(Application.dataPath, fileName);
+}
